5052ebb726ec3b5eaf51a56f1c1658096e671c6e
kbh0581 <kbh0581@gmail.com>
2020-10-28 06:31:35 +0900

    Modify : userInfo array control

--- README.md
+++ README.md
@@ -4,4 +4,6 @@
     > :: UseState()
 * [UseInput_UseRef](https://github.com/kbh0581/react-hooks/blob/main/src/UserInput_UseRef.js) : React Dom variable
     > 컴포넌트내의 Dom 선택자
+    > 리랜더링하지 않을 데이터를 기록 할수도 있음
     > :: UseRef()
+    > ref.current 현재 값 
--- src/UserInput_UseRef.js
+++ src/UserInput_UseRef.js
@@ -1,51 +1,56 @@
 import React,{useRef,useState} from 'react';
 
 
-function User({user}) {
+
+const users = [
+    {
+      id: 1,
+      username: 'kim',
+      email: 'kims@gmail.com'
+      
+    },
+    { 
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com'
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com'
+    }
+];
+
+
+function User({user,onRemove,onToggle}) {
     return <div>
             <span>
-                <label> name :</label> "{user.username}"
+                <label style={{
+                    cursor:'pointer',
+                    color : user.active ? 'green' : 'black'
+                    }}
+                 onClick = {() => {
+                    console.log(user.id)
+                    onToggle(user.id)
+                }}
+                > name :</label> "{user.username}"
             </span>
             <span>
                 <label> email :</label> "{user.email}"
             </span>
+            <button onClick={() => onRemove(user.id)} value="삭제">삭제</button>
         </div>
 }
 
-
-
-function UserList({users}) {
-  
-    return users.map((user)=><User key={user.id} user={user}></User>) 
-    
+function UserList({users,onRemove,onToggle}) {
+      return users.map((user)=><User key={user.id} user={user} onRemove={onRemove} onToggle={onToggle}></User>) 
 }
 
 function UseInput_UseRef(){
     
-   
-    const users = [
-        {
-          id: 1,
-          username: 'kim',
-          email: 'kims@gmail.com'
-          
-        },
-        { 
-          id: 2,
-          username: 'tester',
-          email: 'tester@example.com'
-        },
-        {
-          id: 3,
-          username: 'liz',
-          email: 'liz@example.com'
-        }
-    ];
-
     const [userinfo,setUserInfo] = useState(users);
     const [inputs,setInputs] = useState({userName:'',email:''})
     const nextId = useRef(users.reduce((prev,next)=>next.id+1));
-
     /* useRef는 불변한데이터를 담을때 사용 랜더링이 안되겠끔 및 특정돔을 선택할때 사용함*/
 
     const onChange = (e) =>{
@@ -53,15 +58,29 @@ function UseInput_UseRef(){
         setInputs({...inputs,[name]:value});
     }
 
+    const onRemove = id => {
+        setUserInfo(userinfo.filter((user) => user.id !== id));
+    };  
+
+    const onToggle = id => {
+        setUserInfo(userinfo.map((user)=>{
+            user.active = user.id === id 
+            return user;
+            
+        }));
+    };
+
 
     const onCreate = () => {
         //console.log(inputNameDom.current.value = "");
-
+        if(!inputs.username || !inputs.email) return;
         
         setUserInfo([...userinfo,
                         {id:nextId.current,
                             username:inputs.username,
-                            email:inputs.email}]);
+                            email:inputs.email,
+                            active:false
+                        }]);
         setInputs({username:"",email:""}); 
                            
         nextId.current += 1;
@@ -70,7 +89,7 @@ function UseInput_UseRef(){
 
     return (
         <div>
-        <UserList users={userinfo}></UserList> 
+        <UserList users={userinfo} onRemove={onRemove} onToggle={onToggle}></UserList> 
         <label>username:</label> <input onChange={onChange} value={inputs.username} name="username"></input> 
         <label>email:</label> <input onChange={onChange} value={inputs.email} type="email" name="email"></input> 
         <button  onClick={onCreate}>ADD</button>
