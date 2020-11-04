56aa7f3b45d30e3ce840b9bb803a12442a4ad640
kbh0581 <kbh0581@gmail.com>
2020-11-05 06:31:35 +0900

    add styled-components tuto

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import Button from './components/Button'
+import styled,{css} from 'styled-components'
 import './App.scss';
+
+
+const Cycle = styled.div`
+  width: 5rem;
+  height: 5rem;
+  background : ${(props)=> props.color || 'black'};
+  border-radius : 50%;
+  ${props=> props.huge && css`width :10rem; height: 10rem`}
+  `;
+
+
 
 function App() {
   return (
     <div className="App">
-      <div>
+      <Cycle color="red"/>
+      <Cycle color="yellow"/>
+      <Cycle color="blue"/>
+      <Cycle color="" huge/>
+      {/* <div>
         <Button size="medium">BUTTON</Button>
         <Button size="large" color="pink">BUTTON</Button>
         <Button size="small" color="gray" outline>BUTTON</Button>
@@ -14,7 +30,7 @@ function App() {
         <Button size="medium">BUTTON</Button>
         <Button size="large" color="pink" layout={{"fullWidth":true,"outline":true}} onClick={()=>console.log("PINK")}>BUTTON</Button>
         <Button size="small" color="gray" outline>BUTTON</Button>
-      </div>
+      </div> */}
       
     
     </div>
