85b82e6f0d80fbac7f7205f874148da436058820
nykse <kbh0581@gmai.com>
2020-11-19 00:00:19 +0900

    context + api Call End

--- react-hooks-api/src/componsent/User.js
+++ react-hooks-api/src/componsent/User.js
@@ -1,28 +1,35 @@
 import axios from 'axios';
 import React, { useEffect, useReducer, useState } from 'react';
-
+import {getUser,useUsersDispatch,useUsersState} from './UserContext';
 //import UseAsync from './UseAsync';
 import {useAsync} from 'react-async'
-import { getUser } from './UserContext';
+
 
 
 const API_PATH = process.env.REACT_APP_API_PATH;
 
-async function getUser({id}){
-    const response = await axios.get(`${API_PATH}/users/${id}`);
-    console.log(response);
-    return response.data;
-}
+// async function getUser({id}){
+//     const response = await axios.get(`${API_PATH}/users/${id}`);
+//     console.log(response);
+//     return response.data;
+//}
 
 
 function User({id}){
-  const [state] = UseAsync(() => getUser(id),[id]);
-  const {loading, data: user, error } = state;
-  const {loading,data:user,error,run} = useAsync({
-     promiseFn:getUser,
-     id,
-     watch: id
- })
+//  const [state] = UseAsync(() => getUser(id),[id]);
+  //const {loading, data: user, error } = state;
+//   const {loading,data:user,error,run} = useAsync({
+//      promiseFn:getUser,
+//      id,
+//      watch: id
+//  })
+    const state = useUsersState();
+    const dispatch = useUsersDispatch();    
+    const { data: user, loading, error } = state.user;
+    
+    useEffect(() => {
+        getUser(dispatch, id);
+    }, [dispatch, id]);
     
 
     if (loading) return <div>로딩중...</div>;
