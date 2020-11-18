f24a4543f750a57f7d249abc793bbd887038bd91
kbh0581 <kbh0581@gmai.com>
2020-11-19 00:14:52 +0900

    commit

--- react-hooks-api/src/componsent/api.js
+++ react-hooks-api/src/componsent/api.js
@@ -8,6 +8,6 @@ export async function getUsers() {
 
 export async function getUser(id) {
     const response = await axios.get(`${API_PATH}/users/${id}`);
-    return response.data;
+    return response.data; 
     //추가2
 }
