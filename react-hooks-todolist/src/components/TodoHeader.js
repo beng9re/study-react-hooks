ed5d42c759e63eb5baeb6d76a39602ecb8df4cd9
kbh0581 <kbh0581@gmail.com>
2020-11-23 00:13:49 +0900

    last

--- react-hooks-todolist/src/components/TodoHeader.js
+++ react-hooks-todolist/src/components/TodoHeader.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import {useTodoState} from '../TodoContext';
 
-const TodoHeadBlock = styled.div`
+const TodoHeadBlock = styled.div`  
     padding : 48px 32px 32px 24px;
     border-bottom: 1px solid #e9ecef; 
 
