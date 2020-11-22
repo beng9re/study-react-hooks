12a71fb64dcfca63af7b6fa12e5fc6259cfe066c
kbh0581 <kbh0581.gmail.com>
2020-11-22 23:43:33 +0900

    react-router  -init

---
+++ react-hooks-router/src/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            <h1>
+                홈
+            </h1>
+            <p>홈영역</p>
+            
+        </div>
+    )
+}
+
+
+export default Home;
