b4d948c01d260b5c1c38e8f55875b2d7fb7568f7
kbh0581 <kbh0581@gmail.com>
2020-11-24 23:50:03 +0900

    React Router End

--- react-hooks-router/src/App.js
+++ react-hooks-router/src/App.js
@@ -4,6 +4,8 @@ import { Link, Route } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
 import Profiles from './Profiles';
+import HistorySample from './HistorySample'  
+import WithRouterSample from './WithRouterSample'
 
 function App() {
   return (
@@ -18,12 +20,19 @@ function App() {
         <li>
           <Link to="/profiles">프로필 목록</Link>
         </li> 
+        <li>
+          <Link to="/history">go</Link>
+        </li>
+        
     </ul>
     {/* exact 단일 경로 설정  */}
       <Route path="/" component={Home} exact={true}></Route>
       <Route path="/about" component={About}></Route>
       <Route path="/profiles" component={Profiles} />
+      <Route path="/history" component={HistorySample} />
+      <WithRouterSample />
     </div>
+    
   );
 }
 
